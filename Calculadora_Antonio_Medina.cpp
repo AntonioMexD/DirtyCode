15bb8845098dea3b3e6a71f2b00fe26ba07b9ee6
Stanislao777 <e_vargas.v@outlook.com>
2021-03-29 00:17:23 -0400

    extraccion de metodos largos

--- Calculadora_Antonio_Medina.cpp
+++ Calculadora_Antonio_Medina.cpp
@@ -2,58 +2,69 @@
 
 using namespace std;
 
+int operacionMenu()
+{
+	int operacion;
+	cout << "Calculadora simple" << endl;
+	cout << "1. Suma" << endl;
+	cout << "2. Resta" << endl;
+	cout << "3. Multiplicacion" << endl;
+	cout << "4. Division" << endl;
+	cin >> operacion;
+	return operacion;
+}
+
+void leerNumeros(int& primerNumero, int& segundoNumero)
+{
+	cout << "Ingrese el primer numero" << endl;
+	cin >> primerNumero;
+	cout << "Ingrese el segundo numero" << endl;
+	cin >> segundoNumero;
+}
+
+void leerDecimales(float& primerDecimal, float& segundoDecimal)
+{
+	cout << "Ingrese el primer numero" << endl;
+	cin >> primerDecimal;
+	cout << "Ingrese el segundo numero" << endl;
+	cin >> segundoDecimal;
+}
+
 int main()
 {
 	int primerNumero = 0, segundoNumero = 0;
 	float primerDecimal = 0, segundoDecimal = 0;
-	int op;
+	int operacion;
 	do
-	{
-		cout << "Calculadora simple" << endl;
-		cout << "1. Suma" << endl;
-		cout << "2. Resta" << endl;
-		cout << "3. Multiplicacion" << endl;
-		cout << "4. Division" << endl;
-		cin >> op;
-		if (op == 1)
+	{	
+		operacion = operacionMenu();
+		if (operacion == 1)
 		{
 			cout << "SUMA" << endl;
-			cout << "Ingrese el primer numero" << endl;
-			cin >> primerNumero;
-			cout << "Ingrese el segundo numero" << endl;
-			cin >> segundoNumero;
+			leerNumeros(primerNumero, segundoNumero);
 			cout << primerNumero + segundoNumero << endl;			
 		}
-		if (op == 2)
+		if (operacion == 2)
 		{
 			cout << "RESTA" << endl;
-			cout << "Ingrese el primer numero" << endl;
-			cin >> primerNumero;
-			cout << "Ingrese el segundo numero" << endl;
-			cin >> segundoNumero;
+			leerNumeros(primerNumero, segundoNumero);
 			cout << primerNumero - segundoNumero << endl;			
 		}
-		if (op == 3)
+		if (operacion == 3)
 		{
 			cout << "MULTIPLICACION" << endl;
-			cout << "Ingrese el primer numero" << endl;
-			cin >> primerDecimal;
-			cout << "Ingrese el segundo numero" << endl;
-			cin >> segundoDecimal;
+			leerDecimales(primerDecimal, segundoDecimal);
 			cout << primerDecimal * segundoDecimal << endl;			
 		}
-		if (op == 4)
+		if (operacion == 4)
 		{
 			cout << "DIVISION" << endl;
-			cout << "Ingrese el primer numero" << endl;
-			cin >> primerDecimal;
-			cout << "Ingrese el segundo numero" << endl;
-			cin >> segundoDecimal;
+			leerDecimales(primerDecimal, segundoDecimal);
 			cout << primerDecimal / segundoDecimal << endl;			
 		}
 		system("pause");
 		system("CLS");
-	} while (op!=0);
-
+	} while (operacion!=0);
+	
 	return 0;
 }
